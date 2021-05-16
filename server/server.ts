392062ab72476b1cd4ac69b11dac15e84cea02b9
Oliver Butler <dev@oliverbutler.uk>
2021-05-16 18:52:41 +0100

    💥 Removed Next for reasons

---
+++ server/server.ts
@@ -0,0 +1,12 @@
+import express from "express";
+
+const app = express();
+
+app.use(express.json());
+
+const port = 5000;
+
+app.get("/api/test", (req, res) => res.send("Express + TypeScript Server"));
+app.listen(port, () => {
+  console.log(`⚡️[server]: Server is running at https://localhost:${port}`);
+});
